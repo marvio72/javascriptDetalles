462a8e0ef19910c331952448d08c7ae8279d53b2
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-09-04 12:01:31 -0500

    La Palabra reservada NEW

--- js/app.js
+++ js/app.js
@@ -1,18 +1,56 @@
 
-function Persona(nombre, apellido, edad){
+
+// function Persona(nombre, apellido){
+
+//   this.nombre = nombre;
+//   this.apellido = apellido;
+//   this.edad = 30;
+
+//   this.nombreCompleto = function(){
+//     return this.nombre + " " + this.apellido;  
+//   }
+// }
+
+function Informacion(nombre, apellido, edad, calle, numero, colonia, ciu, estado, pais){
 
   this.nombre = nombre;
   this.apellido = apellido;
   this.edad = edad;
-
-  this.nombreCompleto = function(){
-      return this.nombre + " " + this.apellido;
+  this.nombreCompleto = function() {
+    return nombre+" "+apellido+" "+"("+edad+")";
   }
 
+  this.direccion = {
+    calle: calle,
+    numero: numero,
+    colonia: colonia,
+    direccionCompleta: function() {
+      return calle+" "+numero+" "+colonia;
+    }
+  }
+
+  this.ciudad = {
+    ciu: ciu,
+    estado: estado,
+    pais: pais,
+    ciudadCompleta: function(){
+      return ciu+" "+estado+" "+pais;
+    }
+  }
+   
+  
+  
 }
 
-var juan = new Persona("Marco","Ruvalcaba",47);
 
-console.log(juan);
-console.log(juan.nombre);
-console.log(juan.nombreCompleto());
+// var juan = new Persona("Marco","Ruvalcaba");
+
+var marco = new Informacion("Marco","Ruvalcaba",47,"Av. Compositores",4550,"Los Pinos","Zapopan","Jalisco","México");
+
+// console.log(juan.nombreCompleto());
+
+
+console.log(marco.nombreCompleto());
+console.log(marco.direccion.direccionCompleta());
+
+console.log(marco.nombreCompleto()+" "+marco.direccion.direccionCompleta()+" "+marco.ciudad.ciudadCompleta());
