97882f3f9a96cbcc35260e13214b34fca191b041
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-09-06 17:01:53 -0500

    Funciones anonimas1

--- js/app.js
+++ js/app.js
@@ -1,17 +1,13 @@
+(function(){
+  var a = 10;
 
+  console.log(a);
 
-function Persona(){
-  this.nombre   = "Marco";
-  this.apellido = "Ruvalcaba";
-  this.edad     = 47;
-  this.pais     = "México";
+  function cambiarA(){
+    a=20;
+  }
 
-}
+  cambiarA();
 
-Persona.prototype.imprimirInfo = function(){
-  console.log(this.nombre +" "+ this.apellido +" "+ "("+ this.edad +")");
-}
-var mar = new Persona();
-
-console.log(mar);
-mar.imprimirInfo();
+  console.log(a);
+})();
