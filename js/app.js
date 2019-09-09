5d30d0ec27e0ee960cfa61aa7e6aff27cf89d7a9
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-09-09 16:55:57 -0500

    Arreglos Parte1

--- js/app.js
+++ js/app.js
@@ -1,15 +1,51 @@
 
+var arr = [5,4,3,2,1];
 
-function identifica( param ){
-  console.log(typeof param);
-  console.log(param instanceof Persona);
-}
+console.log(arr[0], arr[4]);
 
-function Persona(){
-  this.nombre = "Marco";
-  this.edad = 30;
-}
+console.log(arr.reverse());
 
-var marco = new Persona();
+//Crea la cantidad elevada al cuadrado
+arr = arr.map(function(elem){
+  elem *= elem;
+  return elem;
+});
+console.log(arr);
 
-identifica(marco);
+// Extrae la raiz cuadrada el valor anterior
+arr = arr.map( Math.sqrt);
+console.log(arr);
+
+// Une todos los elementos en un string con la separación que nosotros le indiquemos
+arr = arr.join("|..");
+console.log(arr);
+
+// Lo contrario a join solo que cada uno de los elementos los devuelve como string
+arr =  arr.split("|..");
+console.log(arr);
+
+// Agrega un elemento a la ultima posición del arreglo
+arr.push("6");
+console.log(arr);
+
+//Ingresa un elemento en la primera posicion del arreglo
+arr.unshift("0");
+console.log(arr);
+
+// Es lo mimso que arr.split() pero mas rápido;
+console.log(arr.toString());
+
+//Con este comando eliminamos el ultimo elemento pero la variable nos indica que elemento fue el eliminado
+var elimine = arr.pop();
+console.log(arr, elimine);
+
+//seleccionamos la posición 1, de ahí no borramos nada(0) pero introducimos el valor 10,20 y 30 despues
+// de la posicion 1
+// Podemos borrar elementos a partir de la posicion que le indiquemos por ejemplo
+// arr.splice(1,3,"10"); esto borra los 3 elementos depues de la posicion 1 e introduce 10,20 y 30.
+arr.splice(1, 0, "10","20","30");
+console.log(arr);
+
+//slice es lo contrario a splice, le podemos indicar un rango para que nos muestre los elementos
+arr = arr.slice(3,7);
+console.log(arr);
