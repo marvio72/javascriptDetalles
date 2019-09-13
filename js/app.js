8c6cb21ade14f87fd3251ac6e53596d54b6f6241
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-09-13 17:06:50 -0500

    Argumentos

--- js/app.js
+++ js/app.js
@@ -1,42 +1,12 @@
+var arguments = 10;
+
+function miFuncion(a,b,c,d){
+  if (arguments.length !== 4) {
+    console.log("la funcion necesita 4 parametros");
+    return;
+  }
+  console.log(a + b + c + d);
+}
 
 
-
-var arr = [
-  true,
-  {
-    nombre:"Marco",
-    apellido:"Ruvalcaba"
-  },
-  function(){
-    console.log("Estoy viviendo en un arreglo");
-  },
-  function(persona){
-    console.log(persona.nombre +" "+ persona.apellido);
-  },
-  ["Fernando","Carlos","Hernando","Melissa",[
-    "Juan",
-    "Pedro",
-    "Dilcia",
-    function(){
-      console.log(this);
-    }
-  ]]
-];
-
-console.log(arr);
-console.log(arr[0]);
-console.log(arr[1].nombre +" "+arr[1].apellido);
-
-arr[2]();
-arr[3](arr[1]);
-
-console.log(arr[4][4][2]);
-
-//Cambiar Pedro por Pedra!ANCHOR 
-var arreglo2 = arr[4][4];
-arreglo2[1] = "Pedra!";
-
-console.log(arreglo2);
-console.log(arr);
-
-arreglo2[3]();
+miFuncion(10, 20, 30, 40);
