95ff184a4dcac6f781fef5fde124efbdc08e34ba
marvio72 <mruvalcaba@oversistemas.com>
2019-09-18 21:56:28 -0500

    Objetos String

--- js/app.js
+++ js/app.js
@@ -1,17 +1,39 @@
 
 
-var a = 10.456789;
-var b = new Number(10);
+var a = new String("Marco Rafael Ruvalcaba Espinoza");
+
+console.log(a);
+
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+
+var i = a.indexOf("Ruvalcaba");
+console.log("La letra esta: " +  i);
+
+i = a.lastIndexOf("n");
+console.log("La letra esta: " +  i);
+
+var nombre = a.substr(6,3);
+console.log(nombre);
+
+nombre = a.substr(0,a.indexOf(" "));
+console.log(nombre);
 
 
-console.log(a.toFixed(2));
-console.log(a.toString());
+var split = a.split(" ");
+console.log(split);
+console.log(split.length);
 
-console.log(a.toPrecision(4));
 
+//Funciones viejas
 
-var b = new Number ("20e");
+function estilo(cadena){
+  return document.write(cadena);
+}
 
-console.log(b);
-console.log(b.valueOf());
+estilo(a.bold());
+estilo(a.blink());
+estilo(a.italics());
+estilo(a.strike());
+
 
