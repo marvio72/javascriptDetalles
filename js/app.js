d7de97ffdaf91a28da297b94f0a62546f573989f
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-09-20 18:37:36 -0500

    Objeto Math

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
-var fecha = new Date(2019,2,7);
+var PI = Math.PI;
+var E = Math.E;
 
-Date.prototype.sumarDias = function(dias){
-  this.setDate( this.getDate() + dias);
-  return this;
-};
+console.log(PI);
+console.log(E);
 
-Date.prototype.sumarAnio = function(anio){
-  this.setFullYear( this.getFullYear() + anio);
-  return this;
-};
+var num1 = 10.6323345;
 
-console.log(fecha);
-console.log(fecha.sumarDias(10));
-console.log(fecha.sumarAnio(20)); 
+console.log(num1);
+console.log(Math.round(num1));
+console.log(Math.floor(num1));
+
+function numeroRandom(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+console.log(numeroRandom(1,6));
+
+console.log(Math.sqrt(9));
+
+console.log(Math.pow(5,3));
