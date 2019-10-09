b66bf764988179d90371bde7bb728014b32a2f5a
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-10-09 17:23:32 -0500

    Eventos 101

--- js/app.js
+++ js/app.js
@@ -1,39 +1,15 @@
 
-//Solo aplica una vez en el tiempo restablecido en milisegundos.
-setTimeout(() => {
-  console.log("Pasaron 3 segundos");
-  intervalo();
-}, 3000);
 
-function intervalo(){
-var i = 1;
-var seg;
-var ciclo = setInterval(() => {
-  if (i == 4) {
-    clearInterval(ciclo);
-  }
-  if (i == 1) {
-    console.log(i +" segundo");
-  }else{
-    console.log(i +" segundos");
-  }
-  i++; 
-}, 1000);
-}
+    function evento(arg){
+      console.log("Me disparé");
+      console.log(arg.x, arg.y);
+    }
 
 
-var marco = {
-  nombre: "Marco",
-  apellido: "Ruvalcaba",
-  edad: 47,
-  imprimir: function(){
-    var self = this;
+    var objeto = document.getElementById("objDemo");
+    
+    objeto.addEventListener("keypress",evento);
 
-    setTimeout(() => {
-      console.log(self);
-      console.log(self.nombre, self.apellido, self.edad);
-    }, 8000);
-  }
-};
+    var boton = document.getElementById("btnDemo");
 
-marco.imprimir();
+    boton.addEventListener("click",evento);
