e1816d6f911b699d58035d01a61ec11db4afbdec
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-10-09 15:43:57 -0500

    Funciones de tiempo

--- js/app.js
+++ js/app.js
@@ -1,18 +1,39 @@
 
-for_principal:
-for (let i = 1; i <= 5; i++) {
-  console.log("i: ", i);
-  
-    for_secundario:
-    for (let w = 1; w <= 5; w++) {
-      console.log("w: ", w); 
+//Solo aplica una vez en el tiempo restablecido en milisegundos.
+setTimeout(() => {
+  console.log("Pasaron 3 segundos");
+  intervalo();
+}, 3000);
 
-      for (let x = 1; x <= 5; x++) {
-        console.log("x: ",x);
-
-        break for_principal;
-        
-      }
-    }
-
+function intervalo(){
+var i = 1;
+var seg;
+var ciclo = setInterval(() => {
+  if (i == 4) {
+    clearInterval(ciclo);
+  }
+  if (i == 1) {
+    console.log(i +" segundo");
+  }else{
+    console.log(i +" segundos");
+  }
+  i++; 
+}, 1000);
 }
+
+
+var marco = {
+  nombre: "Marco",
+  apellido: "Ruvalcaba",
+  edad: 47,
+  imprimir: function(){
+    var self = this;
+
+    setTimeout(() => {
+      console.log(self);
+      console.log(self.nombre, self.apellido, self.edad);
+    }, 8000);
+  }
+};
+
+marco.imprimir();
