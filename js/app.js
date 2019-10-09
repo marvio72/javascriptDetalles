ab601e76f2ae6926ef7ae32d72547117ee8ec3f9
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-10-09 14:16:16 -0500

    Ciclo while y do while

--- js/app.js
+++ js/app.js
@@ -1,20 +1,32 @@
+var i = 1;
+
+while (i <= 10) {
+  console.log(i);
+
+  if (i === 5) {
+    break;
+  }
+  i++;
+}
+
+var j = 0;
+while (j <10) {
+  j++;
+  
+  //Omite el cuatro pero continua con el siguiente.
+  if(j === 4){
+    continue;
+  }
+  console.log(j);
+}
 
 
-var objetoJS = {
-  nombre: "Marco",
-  edad: 47
-};
+// El dowhile ejecuta por lo menos una vez el codigo y despues evalua si continua o no
 
-console.log("Objeto literal", objetoJS);
+var h = 0;
 
+do {
+   h++;
 
-//CONVERTIRLO A MODO JSON
-var jsonString = JSON.stringify(objetoJS);
-
-console.log("Estructura Jason", jsonString);
-
-//CONVERTIRLO DE MODO JSON A OBJETO JS 
-
-var objetoDesdeJson = JSON.parse(jsonString);
-
-console.log("objetoDesdeJason", objetoDesdeJson);
+   console.log(h);  
+} while (h<=10);
